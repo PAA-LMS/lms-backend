2d3cae5070f15800b1133cb8b2061369b314bc97
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2025-02-19 11:43:28 +0530

    first commit set up the back end

--- src/main/java/com/example/lms_backend/LmsBackendApplication.java
+++ src/main/java/com/example/lms_backend/LmsBackendApplication.java
@@ -3,6 +3,7 @@ package com.example.lms_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LmsBackendApplication {
 
